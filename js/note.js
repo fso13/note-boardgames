3052a1f7c946d74c4bc9e5cb326234b615534c4a
Dmitry Rudenko <drudenko@roox.ru>
2022-01-04 23:50:59 +0300

    init3

--- js/note.js
+++ js/note.js
@@ -6,6 +6,12 @@ var notes = [
             "Ну а замки каркасона, строились по четкому плану градостроителей\n" +
             "\n" +
             "А еще появилась монополия)",
-        tags: ["Поселенцы", "Каркасон", "Монополия", "Игротеки"]
+        tags: ["Поселенцы", "Каркасон", "Монополия", "Игротеки"],
+        files: ["https://github.com/fso13/note-boardgames/raw/master/img/1/img_0.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/1/img_1.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/1/img_2.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/1/img_3.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/1/img_4.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/1/img_5.png"]
     }
 ]
