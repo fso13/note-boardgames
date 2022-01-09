59ef3e872d108ded078353e8982cbcf7fedbd0c3
Dmitry Rudenko <dimarudenk@gmail.com>
2022-01-09 18:15:57 +0300

    08.01.22

--- js/note.js
+++ js/note.js
@@ -1,10 +1,25 @@
 var notes = [
     {
+        id: 100,
+        title: "08.01.2022",
+        content: "Первая игра года - Миры Ктулху. Разложили два раза, огонь. </br>" +
+            "Круто что одна коробка до 4-ех человек.</br>" +
+            "Вечер ктулху продолжился новый сценарием Особняков.</br>" +
+            "Сыщики смогли защитить семью пожруги, и заточить монстра.",
+        tags: ["Особняки безумия", "Миры Ктулху", "Игротеки"],
+        files: ["https://github.com/fso13/note-boardgames/raw/master/img/100/AXTeAj0VzzI.jpg",
+            "https://github.com/fso13/note-boardgames/raw/master/img/100/FZHYtHKhWec.jpg",
+            "https://github.com/fso13/note-boardgames/raw/master/img/100/gDoMzSik7ro.jpg",
+            "https://github.com/fso13/note-boardgames/raw/master/img/100/HxdfvxMJS6g.jpg",
+            "https://github.com/fso13/note-boardgames/raw/master/img/100/JO99nLf0kZo.jpg",
+            "https://github.com/fso13/note-boardgames/raw/master/img/100/yuFXJ9Ty4LA.jpg"]
+    },
+    {
         id: 1,
         title: "15.01.2021",
-        content: "Римляне слабаки, зато Варвары в последнем раунде вырвались вперёд, и завоевали победу.\n" +
-            "Ну а замки каркасона, строились по четкому плану градостроителей\n" +
-            "\n" +
+        content: "Римляне слабаки, зато Варвары в последнем раунде вырвались вперёд, и завоевали победу.</br>" +
+            "Ну а замки каркасона, строились по четкому плану градостроителей</br>" +
+            "</br>" +
             "А еще появилась монополия)",
         tags: ["Поселенцы", "Каркасон", "Монополия", "Игротеки"],
         files: ["https://github.com/fso13/note-boardgames/raw/master/img/1/img_0.png",
@@ -13,20 +28,5 @@ var notes = [
             "https://github.com/fso13/note-boardgames/raw/master/img/1/img_3.png",
             "https://github.com/fso13/note-boardgames/raw/master/img/1/img_4.png",
             "https://github.com/fso13/note-boardgames/raw/master/img/1/img_5.png"]
-    },
-    {
-        id: 100,
-        title: "08.01.2022",
-        content: "Первая игра года - Миры Ктулху. Разложили два раза, огонь. \n" +
-            "Круто что одна коробка до 4-ех человек.\n" +
-            "Вечер ктулху продолжился новый сценарием Особняков.\n" +
-            "Сыщики смогли защитить семью пожруги, и заточить монстра.",
-        tags: ["Особняки безумия", "Миры Ктулху", "Игротеки"],
-        files: ["https://github.com/fso13/note-boardgames/raw/master/img/100/AXTeAj0VzzI.jpg",
-                "https://github.com/fso13/note-boardgames/raw/master/img/100/FZHYtHKhWec.jpg",
-                "https://github.com/fso13/note-boardgames/raw/master/img/100/gDoMzSik7ro.jpg",
-                "https://github.com/fso13/note-boardgames/raw/master/img/100/HxdfvxMJS6g.jpg",
-                "https://github.com/fso13/note-boardgames/raw/master/img/100/JO99nLf0kZo.jpg",
-                "https://github.com/fso13/note-boardgames/raw/master/img/100/yuFXJ9Ty4LA.jpg"]
     }
 ]
