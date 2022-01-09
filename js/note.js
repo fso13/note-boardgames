13adee58667025aed83bd3d3eb7b9e12c2646f19
Dmitry Rudenko <dimarudenk@gmail.com>
2022-01-09 17:34:15 +0300

    08.01.22

--- js/note.js
+++ js/note.js
@@ -13,5 +13,20 @@ var notes = [
             "https://github.com/fso13/note-boardgames/raw/master/img/1/img_3.png",
             "https://github.com/fso13/note-boardgames/raw/master/img/1/img_4.png",
             "https://github.com/fso13/note-boardgames/raw/master/img/1/img_5.png"]
+    },
+    {
+        id: 100,
+        title: "08.01.2022",
+        content: "Первая игра года - Миры Ктулху. Разложили два раза, огонь. \n" +
+            "Круто что одна коробка до 4-ех человек.\n" +
+            "Вечер ктулху продолжился новый сценарием Особняков.\n" +
+            "Сыщики смогли защитить семью пожруги, и заточить монстра.",
+        tags: ["Особняки безумия", "Миры Ктулху", "Игротеки"],
+        files: ["https://github.com/fso13/note-boardgames/raw/master/img/100/AXTeAj0VzzI.jpg",
+                "https://github.com/fso13/note-boardgames/raw/master/img/100/FZHYtHKhWec.jpg",
+                "https://github.com/fso13/note-boardgames/raw/master/img/100/gDoMzSik7ro.jpg",
+                "https://github.com/fso13/note-boardgames/raw/master/img/100/HxdfvxMJS6g.jpg",
+                "https://github.com/fso13/note-boardgames/raw/master/img/100/JO99nLf0kZo.jpg",
+                "https://github.com/fso13/note-boardgames/raw/master/img/100/yuFXJ9Ty4LA.jpg"]
     }
 ]
