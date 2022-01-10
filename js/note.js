5cd25a38989c509de9228cb1217cfd35aa00114e
Dmitry Rudenko <drudenko@roox.ru>
2022-01-10 15:43:50 +0300

    add imgs

--- js/note.js
+++ js/note.js
@@ -15,6 +15,177 @@ var notes = [
             "https://github.com/fso13/note-boardgames/raw/master/img/100/yuFXJ9Ty4LA.jpg"]
     },
     {
+        id: 17,
+        title: "24.12.2021",
+        content: "Персонажи пытались сбежать от культистов, не хватило одного спасительного хода... </br>" +
+            "</br>" +
+            "Ну а потом мы пошли мочить монстров. Как давно не играл в манчкин)",
+        tags: ["Особняки безумия", "Манчкин"],
+        files: [
+            "https://github.com/fso13/note-boardgames/raw/master/img/17/unnamed.jpg",
+            "https://github.com/fso13/note-boardgames/raw/master/img/17/unnamed_1.jpg",]
+    },
+    {
+        id: 16,
+        title: "03.12.2021",
+        content: "Вот и окончились приключение, которое длилось два года.</br>" +
+            "Персонажи добились того, к чему стремились. Оставили после себя много чего, изменили мир.</br>" +
+            "Как нибудь ещё вернёмся во Флан, к старым знакомым",
+        tags: ["D&D"],
+        files: [
+            "https://github.com/fso13/note-boardgames/raw/master/img/16/img.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/16/img_1.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/16/img_2.png"]
+    },
+    {
+        id: 15,
+        title: "29.10.2021",
+        content: "Опробовали доп с титанами. Боевики стало намного больше. Осталось в командный режим сыграть.</br>" +
+            "</br>" +
+            "Босс монстр - прикольный филлер. Строишь комнаты с ловушками и монстрами, с определёнными сокровищами, что бы заманить к себе героев.",
+        tags: ["Киклады", "Босс монстр"],
+        files: [
+            "https://github.com/fso13/note-boardgames/raw/master/img/15/img.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/15/img_1.png"]
+    },
+    {
+        id: 14,
+        title: "08.10.2021",
+        content: "Опробовали доп Аид, до титанов не добрались.</br>" +
+            "Но даже так битв было очень много, трое на одного, Харон без работы не остался",
+        tags: ["Киклады"],
+        files: [
+            "https://github.com/fso13/note-boardgames/raw/master/img/14/img.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/14/img_1.png"]
+    },
+    {
+        id: 13,
+        title: "24.09.2021",
+        content: "В подземелье темные эльфы устроили засаду героям.</br>" +
+            "Но они просчитались.",
+        tags: ["D&D"],
+        files: [
+            "https://github.com/fso13/note-boardgames/raw/master/img/13/img.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/13/img_1.png"]
+    },
+    {
+        id: 10,
+        title: "03.09.2021",
+        content: "Два моих топа: алхимики и Киклады.",
+        tags: ["Киклады", "Алхимики"],
+        files: [
+            "https://github.com/fso13/note-boardgames/raw/master/img/10/img.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/10/img_1.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/10/img_2.png"]
+    },
+    {
+        id: 9,
+        title: "20.08.2021",
+        content: "Приключения в подземелье продолжается. Найден убуйца, безумный жрец псевдо поклонник демонов.</br>" +
+            "</br>" +
+            "Ну и пару партий в каркасон)",
+        tags: ["D&D", "Каркасон"],
+        files: [
+            "https://github.com/fso13/note-boardgames/raw/master/img/9/img.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/9/img_1.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/9/img_2.png"]
+    },
+    {
+        id: 11,
+        title: "13.08.2021",
+        content: "Дройды круче всех 🦾",
+        tags: ["ЗВ Внешнее кольцо"],
+        files: [
+            "https://github.com/fso13/note-boardgames/raw/master/img/11/img.png"]
+    },
+    {
+        id: 8,
+        title: "30.07.2021",
+        content: "Diskwars - эта игра ждала 6 лет. Интересный варгейм, будем пробовать ещё.</br>" +
+            "</br>" +
+            "Котики - милейший арт и мозголомный геймплей. Набрал 51 очко, но при равенстве проиграл, меньше котиков прикормил.</br>" +
+            "</br>" +
+            "Фонарики - давно не доставал с полки, но все равно набрать большее количество очков.",
+        tags: ["Diskwars", "Котики", "Фонарики"],
+        files: [
+            "https://github.com/fso13/note-boardgames/raw/master/img/8/img.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/8/img_1.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/8/img_2.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/8/img_3.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/8/img_4.png"]
+    },
+    {
+        id: 7,
+        title: "11.06.2021",
+        content: "Три героя спустились в подземелье, и зачистили его до такой степени, что в конце начали убивать друг друга ради победы.</br>" +
+            "Ну а на Марсе победила строительная компания, хотя озеленители были близки.</br>" +
+            "99 очков",
+        tags: ["Подземелья", "Покорение Марса"],
+        files: [
+            "https://github.com/fso13/note-boardgames/raw/master/img/7/img.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/7/img_1.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/7/img_2.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/7/img_3.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/7/img_4.png"]
+    },
+    {
+        id: 6,
+        title: "28.05.2021",
+        content: "Поляки одержали победу (30 очков от преследователей), хотя их пытались сломить.</br>" +
+            "А вот японцы не дотянули 6 очков до лидера, все решили порушать мне здания.",
+        tags: ["Серп", "Поселенцы"],
+        files: [
+            "https://github.com/fso13/note-boardgames/raw/master/img/6/img.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/6/img_1.png"]
+    },
+    {
+        id: 5,
+        title: "14.05.2021",
+        content: "Четыре героя пытались победить королеву паучиху.</br> Но не смогли, запутались в паутине.",
+        tags: ["Runebound"],
+        files: [
+            "https://github.com/fso13/note-boardgames/raw/master/img/5/img.png"]
+    },
+
+    {
+        id: 4,
+        title: "23.04.2021",
+        content: "Вечер филлеров.",
+        tags: ["Каркасон", "Наместник", "Саграда", "Поселенцы"],
+        files: [
+            "https://github.com/fso13/note-boardgames/raw/master/img/4/img.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/4/img_1.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/4/img_2.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/4/img_3.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/4/img_4.png"]
+    },
+
+    {
+        id: 3,
+        title: "26.03.2021",
+        content: "Наконец-то провёл сессию, полгода откладывал.</br>" +
+            "Надо находить баланс между боевками и социалкой, а то даже я устал кидать Кубы.</br>" +
+            "Но в принципе прошло весело, главное что собрались",
+        tags: ["D&D"],
+        files: [
+            "https://github.com/fso13/note-boardgames/raw/master/img/3/img.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/3/img_1.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/3/img_2.png"]
+    },
+    {
+        id: 2,
+        title: "05.03.2021",
+        content: "В первый раз у троих игроков одинаковое количество очков (51), пришлось устраивать взрыв для определения победителя. А итоге победил Я)</br>" +
+            "Ну А в звездных войнах тактика диверсий и атак на патрули принесла мне победу.",
+        tags: ["Взрывная лаборатория", "ЗВ Внешнее кольцо"],
+        files: [
+            "https://github.com/fso13/note-boardgames/raw/master/img/2/img.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/2/img_1.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/2/img_2.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/2/img_3.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/2/img_4.png"]
+    },
+    {
         id: 1,
         title: "15.01.2021",
         content: "Римляне слабаки, зато Варвары в последнем раунде вырвались вперёд, и завоевали победу.</br>" +
@@ -28,5 +199,14 @@ var notes = [
             "https://github.com/fso13/note-boardgames/raw/master/img/1/img_3.png",
             "https://github.com/fso13/note-boardgames/raw/master/img/1/img_4.png",
             "https://github.com/fso13/note-boardgames/raw/master/img/1/img_5.png"]
+    },
+    {
+        id: 0,
+        title: "08.01.2021",
+        content: 'Дам второй шанс этой игре.</br> Тем более по скидке)',
+        tags: ["Поселенцы"],
+        files: [
+            "https://github.com/fso13/note-boardgames/raw/master/img/0/img.png",
+            "https://github.com/fso13/note-boardgames/raw/master/img/0/img_1.png"]
     }
 ]
